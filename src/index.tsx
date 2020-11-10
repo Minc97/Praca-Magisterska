fdc6d1fc5764b39212c58d6737f1678cd85f4396
Mateusz <mateusz.mikulski97@gmail.com>
2020-11-10 20:22:42 +0100

    Dodatnie sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { Provider } from 'react-redux'
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import store from "./redux/store";
 import App from "./App";
+
+Sentry.init({
+  dsn: "https://23a64f7b59c64556bb9c2e72bcf7ce02@o474776.ingest.sentry.io/5511611",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
